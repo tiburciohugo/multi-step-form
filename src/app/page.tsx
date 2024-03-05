44a6ddedd064f05ceefe9a4c566d22629fb51fd8
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-03-05 20:54:59 -0300

    feat: add navbar mobile styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import PersonalInfo from "@/components/personalInfo";
 import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
-    <main className="bg-marine-blue w-screen h-screen">
+    <main className="bg-blue-50 w-screen h-screen">
       <Navbar />
+      <PersonalInfo />
     </main>
   );
 }
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,21 +1,53 @@
 import Link from "next/link";
 import React from "react";
+import BgSidebarMobile from "../../../public/assets/images/bg-sidebar-mobile.svg";
+import BgSidebarDesktop from "../../../public/assets/images/bg-sidebar-desktop.svg";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="flex gap-4 text-white font-bold group">
-      <Link className="ring-white ring-1 rounded-full w-7 h-7 text-center hover:text-marine-blue hover:bg-light-blue hover:ring-light-blue transition ease-in-out delay-200" href="/1">
-        1
-      </Link>
-      <Link className="ring-white ring-1 rounded-full w-7 h-7 text-center hover:text-marine-blue hover:bg-light-blue hover:ring-light-blue transition ease-in-out delay-200" href="/2">
-        2
-      </Link>
-      <Link className="ring-white ring-1 rounded-full w-7 h-7 text-center hover:text-marine-blue hover:bg-light-blue hover:ring-light-blue transition ease-in-out delay-200" href="/3">
-        3
-      </Link>
-      <Link className="ring-white ring-1 rounded-full w-7 h-7 text-center hover:text-marine-blue hover:bg-light-blue hover:ring-light-blue transition ease-in-out delay-200" href="/4">
-        4
-      </Link>
+      <div className="">        
+        <Image
+          className="w-screen block md:hidden"
+          src={BgSidebarMobile}
+          alt="sidebar"
+        />
+        <Image className="hidden md:block" src={BgSidebarDesktop} alt="sidebar" />
+        {/* <img
+          className="hidden md:block"
+          src={BgSidebarDesktop}
+          alt="sidebar"
+        /> */}
+      </div>
+
+
+      <div className="absolute w-full mx-auto flex-1 flex justify-center py-10 space-x-5">
+        <Link
+          className="ring-white ring-1 rounded-full w-7 h-7 text-center hover:text-marine-blue hover:bg-light-blue hover:ring-light-blue transition ease-in-out delay-100"
+          href="/1"
+        >
+          1
+        </Link>
+        <Link
+          className="ring-white ring-1 rounded-full w-7 h-7 text-center hover:text-marine-blue hover:bg-light-blue hover:ring-light-blue transition ease-in-out delay-100"
+          href="/2"
+        >
+          2
+        </Link>
+        <Link
+          className="ring-white ring-1 rounded-full w-7 h-7 text-center hover:text-marine-blue hover:bg-light-blue hover:ring-light-blue transition ease-in-out delay-100"
+          href="/3"
+        >
+          3
+        </Link>
+        <Link
+          className="ring-white ring-1 rounded-full w-7 h-7 text-center hover:text-marine-blue hover:bg-light-blue hover:ring-light-blue transition ease-in-out delay-100"
+          href="/4"
+        >
+          4
+        </Link>
+      </div>
     </nav>
   );
 }
